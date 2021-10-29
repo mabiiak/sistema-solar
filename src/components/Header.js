5fba4f57044007040ede984eab1a4d5d305c2beb
mabiane <mabiane.polniak@gmail.com>
2021-10-29 15:38:06 -0300

    add Component Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// renderizar titulo da pagina principal
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
