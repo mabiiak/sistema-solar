9e255a43097e33fa11c16d913e482c516052eb2b
mabiane <mabiane.polniak@gmail.com>
2021-10-29 19:40:17 -0300

    Planet Card implementada em SolarSystem

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// O componente PlanetCard deve receber duas props:
-// uma chamada planetName e outra chamada planetImage
-
 class PlanetaCard extends Component {
+  PropTypes = {
+    planetName: PropTypes.string.isRequired,
+  }
+
   render() {
     const { planetName, planetImage } = this.props;
+
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">
           {planetName}
         </p>
-        <img src={planetImage} alt={`Planeta ${planetName}`} />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
+
+PlanetaCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetaCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetaCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
@@ -7,6 +9,15 @@ class SolarSystem extends Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+
+        <PlanetaCard
+          planetName={
+            planets.map((planet) => planet.name)
+          }
+          planetImage={
+            planets.map((planet) => planet.image)
+          }
+        />
       </>
     );
   }
