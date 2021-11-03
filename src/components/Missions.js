783afe91cf468c9a91b8ee4eacc26e75b0e77ac4
mabiane <mabiane.polniak@gmail.com>
2021-11-03 10:50:43 -0300

    renderizado title em missions

---
+++ src/components/Missions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class Missions extends Component {
+  render() {
+    return (
+      <>
+        <div data-testid="missions">aaaa</div>
+        <Title headline="Missões" />
+      </>
+    );
+  }
+}
+
+export default Missions;
