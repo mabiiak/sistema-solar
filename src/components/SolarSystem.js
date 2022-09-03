330bbff1236e43b32f58456d791755925a4b26c2
mabiiak <mabiane.polniak@gmail.com>
2022-09-03 16:02:38 -0300

    refactor: reorder files

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import PlanetaCard from './PlanetCard';
+import planets from '../data/planets';
+import './style.css';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div
+        data-testid="solar-system"
+      >
+        <Title
+          headline="Planetas"
+        />
+        <div id="bg-cards">
+          {
+            planets.map((planet) => (
+              <PlanetaCard
+                key={ planet.name }
+                planetName={ planet.name }
+                planetImage={ planet.image }
+              />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
